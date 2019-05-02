279fa313712e1e221c4aa85202880748c8d603c8
Lucian Boaca <lucianboaca97@gmail.com>
2019-05-02 17:14:15 +0300

    Added a scanner activity. Updated the theme to make give it a black background and translucent top bar.

---
+++ ReceiptScan/app/src/main/java/com/lucianbc/receiptscan/view/activity/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.lucianbc.receiptscan.view.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lucianbc.receiptscan.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        scan_btn.setOnClickListener {
+            startActivity(Intent(this, Scanner::class.java))
+        }
+    }
+}
--- ReceiptScan/app/src/main/java/com/lucianbc/receiptscan/view/activity/Scanner.kt
+++ ReceiptScan/app/src/main/java/com/lucianbc/receiptscan/view/activity/Scanner.kt
@@ -1,12 +1,13 @@
-package com.lucianbc.receiptscan
+package com.lucianbc.receiptscan.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lucianbc.receiptscan.R
 
-class MainActivity : AppCompatActivity() {
+class Scanner : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_scanner)
     }
 }
