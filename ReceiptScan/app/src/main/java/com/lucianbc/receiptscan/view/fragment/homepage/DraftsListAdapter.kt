46c3fcd41b6a5e64f362f1b9898e2ff6683ff4b0
Lucian Boaca <lucianboaca97@gmail.com>
2019-05-15 09:58:47 +0300

    Modified the draft item layout.

--- ReceiptScan/app/src/main/java/com/lucianbc/receiptscan/view/fragment/homepage/DraftsListAdapter.kt
+++ ReceiptScan/app/src/main/java/com/lucianbc/receiptscan/view/fragment/homepage/DraftsListAdapter.kt
@@ -3,15 +3,17 @@ package com.lucianbc.receiptscan.view.fragment.homepage
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.lucianbc.receiptscan.R
 import com.lucianbc.receiptscan.domain.model.ReceiptDraft
+import kotlinx.android.synthetic.main.draft_list_item.view.*
 
-class DraftsListAdapter (
-    private val clickCallback: (ReceiptDraft) -> Unit
-) : ListAdapter<ReceiptDraft, DraftsListAdapter.DraftViewHolder>(ReceiptDiffCallback()) {
+class DraftsListAdapter(
+    private val clickListener: (ReceiptDraft) -> Unit
+): ListAdapter<ReceiptDraft, DraftsListAdapter.DraftViewHolder>(ReceiptDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DraftViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -20,14 +22,16 @@ class DraftsListAdapter (
     }
 
     override fun onBindViewHolder(holder: DraftViewHolder, position: Int) {
-        holder.bind(getItem(position))
+        holder.bind(getItem(position), clickListener, position)
     }
 
     inner class DraftViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        fun bind(element: ReceiptDraft) {
-            this.itemView.setOnClickListener {
-                clickCallback(element)
-            }
+        private var posTbox: TextView = view.item_pos
+        private val contTbox: TextView = view.item_content
+        fun bind(element: ReceiptDraft, listener: (ReceiptDraft) -> Unit, position: Int) {
+            posTbox.text = position.toString()
+            contTbox.text = "${element.id.toString()} - ${element.imagePath}"
+            this.itemView.setOnClickListener { listener(element) }
         }
     }
 }
