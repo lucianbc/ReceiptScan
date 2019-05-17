90274608a3821fbf0ac0aeab3e6d866478f25dde
Lucian Boaca <lucianboaca97@gmail.com>
2019-05-17 10:16:36 +0300

    Replaced observe with data binding.

--- ReceiptScan/app/src/main/java/com/lucianbc/receiptscan/view/fragment/review/DraftImage.kt
+++ ReceiptScan/app/src/main/java/com/lucianbc/receiptscan/view/fragment/review/DraftImage.kt
@@ -1,15 +1,18 @@
 package com.lucianbc.receiptscan.view.fragment.review
 
 
+import android.graphics.Bitmap
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.BindingAdapter
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
-
+import com.github.chrisbanes.photoview.PhotoView
 import com.lucianbc.receiptscan.R
 import com.lucianbc.receiptscan.base.BaseFragment
+import com.lucianbc.receiptscan.databinding.FragmentDraftImageBinding
 import com.lucianbc.receiptscan.viewmodel.DraftReviewViewModel
 import kotlinx.android.synthetic.main.fragment_draft_image.*
 
@@ -20,7 +23,18 @@ class DraftImage:
         savedInstanceState: Bundle?
     ): View? {
         initParentViewModel()
-        return inflater.inflate(R.layout.fragment_draft_image, container, false)
+
+        val binding = DataBindingUtil.inflate<FragmentDraftImageBinding>(
+            inflater,
+            R.layout.fragment_draft_image,
+            container,
+            false
+        )
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -30,4 +44,9 @@ class DraftImage:
             draft_photo_view.setImageBitmap(it)
         })
     }
+}
+
+@BindingAdapter("app:bitmap")
+fun bindImage(view: PhotoView, image: Bitmap?) {
+    view.setImageBitmap(image)
 }
