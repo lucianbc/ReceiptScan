1c08fcc43cd106573318bfac3f03d7d64a2fe641
Lucian Boaca <lucianboaca97@gmail.com>
2019-05-28 11:38:23 +0300

    Removed the navigation component.

--- ReceiptScan/app/src/main/java/com/lucianbc/receiptscan/MainActivity.kt
+++ ReceiptScan/app/src/main/java/com/lucianbc/receiptscan/MainActivity.kt
@@ -2,11 +2,35 @@ package com.lucianbc.receiptscan
 
 import android.os.Bundle
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : DaggerAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initAdapter()
+        initButtons()
+    }
+
+    private fun initAdapter() {
+        supportFragmentManager.apply {
+            HomePagerAdapter(this).also {
+                pager.adapter = it
+                pager.currentItem = HomePagerAdapter.DRAFTS
+            }
+        }
+    }
+
+    private fun initButtons() {
+        drafts_btn.setOnClickListener { setView(HomePagerAdapter.DRAFTS) }
+        export_btn.setOnClickListener { setView(HomePagerAdapter.EXPORT) }
+        receipts_btn.setOnClickListener { setView(HomePagerAdapter.RECEIPTS) }
+        settings_btn.setOnClickListener { setView(HomePagerAdapter.SETTINGS) }
+    }
+
+    private fun setView(pos: Int) {
+        pager.setCurrentItem(pos, false)
     }
 }
