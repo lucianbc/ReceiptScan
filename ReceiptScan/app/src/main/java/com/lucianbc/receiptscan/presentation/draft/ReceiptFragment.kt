9a8820caccfa0ad4c7ae97379a2d5da1862444fa
Lucian Boaca <lucianboaca97@gmail.com>
2019-06-03 17:51:08 +0300

    :lipstick: Removed imports.

--- ReceiptScan/app/src/main/java/com/lucianbc/receiptscan/presentation/draft/ReceiptFragment.kt
+++ ReceiptScan/app/src/main/java/com/lucianbc/receiptscan/presentation/draft/ReceiptFragment.kt
@@ -8,12 +8,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.lucianbc.receiptscan.R
-import com.lucianbc.receiptscan.di.DaggerAwareViewModelFactory
-import com.lucianbc.receiptscan.domain.model.Product
 import com.lucianbc.receiptscan.domain.model.ProductDraft
-import dagger.android.support.DaggerFragment
 import kotlinx.android.synthetic.main.receipt_layout.*
-import javax.inject.Inject
 
 
 class ReceiptFragment : Fragment() {
