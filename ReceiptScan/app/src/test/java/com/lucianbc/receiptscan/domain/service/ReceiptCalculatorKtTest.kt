89b7890bd3d15417cebe5a9b6bf3bb15d950cee4
Lucian Boaca <lucianboaca97@gmail.com>
2019-06-05 14:19:48 +0300

    Implemented a noise tolerant date parsing method.

--- ReceiptScan/app/src/test/java/com/lucianbc/receiptscan/domain/service/ReceiptCalculatorKtTest.kt
+++ ReceiptScan/app/src/test/java/com/lucianbc/receiptscan/domain/service/ReceiptCalculatorKtTest.kt
@@ -2,6 +2,7 @@ package com.lucianbc.receiptscan.domain.service
 
 import org.junit.Assert.*
 import org.junit.Test
+import java.text.SimpleDateFormat
 
 class ReceiptCalculatorKtTest {
     @Test
@@ -9,5 +10,13 @@ class ReceiptCalculatorKtTest {
         val subject = "2 BUC. X 22.00 44.00C"
         val result = parseNumber(subject)
         assertEquals(44F, result)
+    }
+
+    @Test
+    fun testFindDate() {
+        val subject = "DATA: 04-05-2019 ORA: 20:34:04"
+        val result = parseDate(subject)
+        val expected = SimpleDateFormat("dd-MM-yyyy").parse("04-05-2019")
+        assertEquals(expected, result)
     }
 }
