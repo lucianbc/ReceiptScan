4cefd9576d0f2be3735042f0ef31d479703a8863
Lucian Boaca <lucianboaca97@gmail.com>
2019-08-06 22:15:18 +0300

    Category fragment and layouts.

---
+++ app/src/main/java/com/lucianbc/receiptscan/presentation/draft/CategoryFragment.kt
@@ -0,0 +1,21 @@
+package com.lucianbc.receiptscan.presentation.draft
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.lucianbc.receiptscan.R
+
+
+class CategoryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_category, container, false)
+    }
+
+}
