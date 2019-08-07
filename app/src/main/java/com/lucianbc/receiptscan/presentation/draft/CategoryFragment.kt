33842ecfcd65892f90527d10b8ecc5e65adb73b1
Lucian Boaca <lucianboaca97@gmail.com>
2019-08-07 11:27:32 +0300

    Implemented categories adapter.

--- app/src/main/java/com/lucianbc/receiptscan/presentation/draft/CategoryFragment.kt
+++ app/src/main/java/com/lucianbc/receiptscan/presentation/draft/CategoryFragment.kt
@@ -5,11 +5,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.lucianbc.receiptscan.R
+import com.lucianbc.receiptscan.domain.model.Category
+import kotlinx.android.synthetic.main.fragment_category.*
 
 
-class CategoryFragment : Fragment() {
+class CategoryFragment(
+    private val callback: (Category) -> Unit
+) : Fragment() {
+
+    private lateinit var _adapter: CategoryAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,4 +31,97 @@ class CategoryFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_category, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _adapter = CategoryAdapter {
+            callback.invoke(it)
+            fragmentManager?.popBackStack()
+        }
+        categoriesList.apply {
+            adapter = _adapter
+            layoutManager = GridLayoutManager(activity, 3)
+        }
+        (currencyToolbar.menu.getItem(0).actionView as SearchView)
+            .apply(::initSearch)
+    }
+
+    private fun initSearch(view: SearchView) {
+        view.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?) = false
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                _adapter.filter.filter(newText)
+                 return false
+            }
+        })
+    }
+}
+
+private class CategoryAdapter(
+    private val callback: (Category) -> Unit
+) : ListAdapter<Category, CategoryAdapter.Holder>(Diff()), Filterable {
+
+    private var allItems = emptyList<Category>()
+
+    init {
+        submitList(Category.values().asList())
+    }
+
+    override fun submitList(list: List<Category>?) {
+        list?.let { allItems = list }
+        super.submitList(list)
+    }
+
+    private fun submitWithoutSave(list: List<Category>?) = super.submitList(list)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.category_item_layout, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        val element = getItem(position)
+        holder.item = element
+    }
+
+    override fun getFilter() = object: Filter() {
+        override fun performFiltering(constraint: CharSequence?): FilterResults {
+            return FilterResults().apply { values = filterCategories(constraint, allItems) }
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+            submitWithoutSave(results?.values as List<Category>?)
+        }
+    }
+
+    inner class Holder(val view: View) : RecyclerView.ViewHolder(view) {
+        var item : Category? = null
+            set(value) {
+                field = value
+                view.setOnClickListener {
+                    value?.let { callback.invoke(value) } }
+            }
+    }
+
+    class Diff : DiffUtil.ItemCallback<Category>() {
+        override fun areItemsTheSame(oldItem: Category, newItem: Category): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Category, newItem: Category): Boolean {
+            return oldItem.name == newItem.name
+        }
+    }
+}
+
+private fun filterCategories(constraint: CharSequence?, items: List<Category>): List<Category> {
+    return if (constraint == null || constraint.isEmpty())
+            items
+    else {
+        val const = constraint.toString().toLowerCase().trim()
+        items.filter { category -> category.name.toLowerCase().contains(const) }
+    }
 }
