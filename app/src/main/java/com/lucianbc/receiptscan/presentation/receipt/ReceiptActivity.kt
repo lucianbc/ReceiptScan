0809fe5cd92825723a1198affdee9feb46137ff7
Lucian Boaca <lucianboaca97@gmail.com>
2019-08-08 14:52:34 +0300

    Added the fragment and the nav intent.

--- app/src/main/java/com/lucianbc/receiptscan/presentation/receipt/ReceiptActivity.kt
+++ app/src/main/java/com/lucianbc/receiptscan/presentation/receipt/ReceiptActivity.kt
@@ -1,5 +1,7 @@
 package com.lucianbc.receiptscan.presentation.receipt
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.lucianbc.receiptscan.R
@@ -9,5 +11,11 @@ class ReceiptActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receipt)
+    }
+
+    companion object {
+        fun navIntent(
+            context: Context
+        ) = Intent (context, ReceiptActivity::class.java)
     }
 }
