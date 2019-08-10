31d3c11f498bc78a0b49ca9e031a49fac1d19cf3
Lucian Boaca <lucianboaca97@gmail.com>
2019-08-10 15:08:25 +0300

    Added callbacks and dismiss on tap.

--- app/src/main/java/com/lucianbc/receiptscan/presentation/ShareOptionsSheet.kt
+++ app/src/main/java/com/lucianbc/receiptscan/presentation/ShareOptionsSheet.kt
@@ -7,10 +7,31 @@ import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.lucianbc.receiptscan.R
+import kotlinx.android.synthetic.main.share_options_layout.*
 
 class ShareOptionsSheet : BottomSheetDialogFragment() {
+    var onTextOnly: (() -> Unit)? = null
+    var onImageOnly: (() -> Unit)? = null
+    var onBoth: (() -> Unit)? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.share_options_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        textOnlyOption.setOnClickListener {
+            onTextOnly?.invoke()
+            dismiss()
+        }
+        imageOnlyOption.setOnClickListener {
+            onImageOnly?.invoke()
+            dismiss()
+        }
+        bothOption.setOnClickListener {
+            onBoth?.invoke()
+            dismiss()
+        }
     }
 
     companion object {
