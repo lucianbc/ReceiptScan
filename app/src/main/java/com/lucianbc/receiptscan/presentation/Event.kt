c13918a2b07ec249e11cda8c8f6d76bff84d04df
Lucian Boaca <lucianboaca97@gmail.com>
2019-08-14 16:15:12 +0300

    Inheritance in sealed class.

--- app/src/main/java/com/lucianbc/receiptscan/presentation/Event.kt
+++ app/src/main/java/com/lucianbc/receiptscan/presentation/Event.kt
@@ -3,6 +3,6 @@ package com.lucianbc.receiptscan.presentation
 import com.otaliastudios.cameraview.PictureResult
 
 sealed class Event {
-    data class PictureTaken(val pictureResult: PictureResult)
-    data class ImageScanned(val draftId: Long)
+    data class PictureTaken(val pictureResult: PictureResult) : Event()
+    data class ImageScanned(val draftId: Long) : Event()
 }
