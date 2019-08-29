eb1abe159645ac572131f9c84f8833521ade56aa
Lucian Boaca <lucianboaca97@gmail.com>
2019-08-29 15:32:54 +0300

    Marked the export as complete on receiving the notification.

--- app/src/main/java/com/lucianbc/receiptscan/infrastructure/services/NotificationService.kt
+++ app/src/main/java/com/lucianbc/receiptscan/infrastructure/services/NotificationService.kt
@@ -1,16 +1,34 @@
 package com.lucianbc.receiptscan.infrastructure.services
 
+import android.annotation.SuppressLint
+import android.app.PendingIntent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.lucianbc.receiptscan.R
 import com.lucianbc.receiptscan.infrastructure.dao.PreferencesDao
+import com.lucianbc.receiptscan.presentation.home.HomePagerAdapter
+import com.lucianbc.receiptscan.presentation.home.MainActivity
+import com.lucianbc.receiptscan.presentation.home.exports.CHANNEL_ID
+import com.lucianbc.receiptscan.presentation.home.exports.ExportUseCase
 import com.lucianbc.receiptscan.util.logd
+import com.lucianbc.receiptscan.util.loge
+import com.lucianbc.receiptscan.util.logi
 import dagger.android.AndroidInjection
+import io.reactivex.Completable
+import io.reactivex.Single
+import io.reactivex.rxkotlin.zipWith
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class NotificationService : FirebaseMessagingService() {
 
     @Inject
     lateinit var preferencesDao: PreferencesDao
+
+    @Inject
+    lateinit var exportUseCase: ExportUseCase
 
     override fun onCreate() {
         super.onCreate()
@@ -19,8 +37,9 @@ class NotificationService : FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
-        logd("Received notification from firebase")
-        logd("Payload was ${message.data}")
+        logi("Received message!")
+        showNotification()
+        handleMessage(message)
     }
 
     override fun onNewToken(token: String) {
@@ -28,5 +47,46 @@ class NotificationService : FirebaseMessagingService() {
         logd("Generated new token")
         logd("Here it is: $token")
         preferencesDao.setNotificationToken(token)
+    }
+
+    @SuppressLint("CheckResult")
+    private fun handleMessage(message: RemoteMessage) {
+        val exportId = Single.fromCallable {
+            message.data["requestId"] ?: throw IllegalArgumentException("RequestId was null")
+        }
+        val downloadUrl = Single.fromCallable {
+            message.data["url"] ?: throw IllegalArgumentException("Download Url was null")
+        }
+
+        exportId
+            .zipWith(downloadUrl)
+            .observeOn(Schedulers.io())
+            .flatMapCompletable { (exportId, downloadUrl) ->
+                exportUseCase.markAsFinished(exportId, downloadUrl)
+            }
+            .andThen(showNotification())
+            .doOnComplete {
+                logi("Message successfuly handled")
+            }
+            .doOnError {
+                loge("Error handling message", it)
+            }
+            .subscribe()
+    }
+
+    private fun showNotification(): Completable {
+        val tapIntent = MainActivity.navIntent(this, HomePagerAdapter.EXPORT)
+        val pendingIntent = PendingIntent.getActivity(this, 0, tapIntent, 0)
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Export finished")
+            .setContentText("Your files are ready to be downloaded")
+            .setSmallIcon(R.drawable.ic_android_black_24dp)
+            .setContentIntent(pendingIntent)
+            .build()
+        return Completable.fromCallable {
+            with(NotificationManagerCompat.from(this)) {
+                notify(1, notification)
+            }
+        }
     }
 }
--- app/src/main/java/com/lucianbc/receiptscan/presentation/home/exports/ExportUseCase.kt
+++ app/src/main/java/com/lucianbc/receiptscan/presentation/home/exports/ExportUseCase.kt
@@ -1,10 +1,17 @@
 package com.lucianbc.receiptscan.presentation.home.exports
 
 import com.lucianbc.receiptscan.domain.export.ExportRepository
+import com.lucianbc.receiptscan.domain.export.Status
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class ExportUseCase @Inject constructor(
-   val repository: ExportRepository
+    val repository: ExportRepository
 ) {
     fun list() = repository.linst()
+
+    fun markAsFinished(id: String, downloadUrl: String) =
+        repository
+            .updateStatus(id, Status.COMPLETE, downloadUrl)
+            .subscribeOn(Schedulers.io())
 }
