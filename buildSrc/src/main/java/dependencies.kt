12b67c9f4fce39c2e8daf2f68af1faa1cac24926
lucianbc <lucianboaca97@gmail.com>
2020-01-27 23:37:22 +0000

    Added new module and buildSrc module for multi-module project.

---
+++ buildSrc/src/main/java/dependencies.kt
@@ -0,0 +1,17 @@
+package com.lucianbc.receiptscan.buildsrc
+
+import org.gradle.api.JavaVersion
+
+object Config {
+    const val minSdk = 23
+    const val compileSdk = 28
+    const val targetSdk = 28
+    val javaVersion = JavaVersion.VERSION_1_8
+    const val buildTools = "28.0.3"
+}
+
+object Dependencies {
+    object Google {
+        const val material = "com.google.android.material:material:1.2.0-alpha04"
+    }
+}
---
+++ ui-homescreen/src/main/java/com/lucianbc/receiptscan/ui/homescreen/HomeScreenView.kt
@@ -0,0 +1,15 @@
+package com.lucianbc.receiptscan.ui.homescreen
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+
+class HomeScreenView(context: Context, attrs: AttributeSet) : CoordinatorLayout(context, attrs) {
+    object Something {
+        fun comp() {
+
+        }
+    }
+
+
+}
