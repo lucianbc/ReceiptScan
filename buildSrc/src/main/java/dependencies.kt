cb8c80990d4e7a9677cc37594537cc6b886e3622
lucianbc <lucianboaca97@gmail.com>
2020-02-02 14:21:40 +0000

    Added navigation components.

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -14,4 +14,10 @@ object Dependencies {
     object Google {
         const val material = "com.google.android.material:material:1.2.0-alpha04"
     }
+
+    object Androidx {
+        private const val navVersion = "2.2.0"
+        const val navigationFragment = "androidx.navigation:navigation-fragment-ktx:${navVersion}"
+        const val navigationUi = "androidx.navigation:navigation-ui-ktx:${navVersion}"
+    }
 }
