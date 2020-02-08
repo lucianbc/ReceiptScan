4be8f6bbe4883a1f4e37192da674021d8ab33d7e
lucianbc <lucianboaca97@gmail.com>
2020-02-08 16:07:44 +0000

    Added safeargs navigation library. Linked all the 3 fragments.

--- receiptscan/src/main/java/com/lucianbc/receiptscan/MainActivity.kt
+++ receiptscan/src/main/java/com/lucianbc/receiptscan/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.lucianbc.receiptscan
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val navController by lazy {
+        Navigation.findNavController(this, R.id.nav_host_fragment)
+    }
+    private val appBarConfig = AppBarConfiguration.Builder(setOf(
+        R.id.destination_transactions
+    )).build()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupBottomNavMenu()
+        setupActionBar()
+    }
+
+    private fun setupBottomNavMenu() {
+        bottom_navigation?.let {
+            NavigationUI.setupWithNavController(it, navController)
+        }
+    }
+
+    private fun setupActionBar() {
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, appBarConfig)
     }
 }
--- receiptscan/src/main/java/com/lucianbc/receiptscan/PendingFragment.kt
+++ receiptscan/src/main/java/com/lucianbc/receiptscan/PendingFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_pending.*
 
 
 class PendingFragment : Fragment() {
@@ -15,5 +17,13 @@ class PendingFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pending, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        pendingItemBtn.setOnClickListener {
+            val dest = PendingFragmentDirections.nextAction()
+            Navigation.findNavController(it).navigate(dest)
+        }
     }
 }
