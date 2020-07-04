924aa1ecab6c7228ef94975c94d4962a704efd4c
Lucian Boaca <lucianboaca97@gmail.com>
2020-07-04 14:59:58 +0100

    Added basic fragment navigation.

---
+++ receiptscan/src/main/java/com/lucianbc/receiptscan/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.lucianbc.receiptscan
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button.setOnClickListener {
+            navigate()
+        }
+    }
+
+    private fun navigate() {
+        val action = HomeFragmentDirections.actionHomeToScanner()
+        this.findNavController().navigate(action)
+    }
+}
