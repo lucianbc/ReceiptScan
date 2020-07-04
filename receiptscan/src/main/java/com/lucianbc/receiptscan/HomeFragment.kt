3a2924ff67bf8c8e4450d7b3b106084c269ce245
Lucian Boaca <lucianboaca97@gmail.com>
2020-07-04 22:23:43 +0100

    Added some basic home navigation.

--- receiptscan/src/main/java/com/lucianbc/receiptscan/HomeFragment.kt
+++ receiptscan/src/main/java/com/lucianbc/receiptscan/HomeFragment.kt
@@ -1,11 +1,17 @@
 package com.lucianbc.receiptscan
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.lucianbc.receiptscan.home.DashboardFragment
+import com.lucianbc.receiptscan.home.ExportsFragment
+import com.lucianbc.receiptscan.home.PendingFragment
+import com.lucianbc.receiptscan.home.SettingsFragment
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
@@ -23,14 +29,38 @@ class HomeFragment : Fragment() {
         setNavigation()
     }
 
-    private fun setNavigation() {
-        button.setOnClickListener {
-            HomeFragmentDirections.actionHomeToScanner()
-                .let(this.findNavController()::navigate)
-        }
-        button2.setOnClickListener {
-            HomeFragmentDirections.actionHomeToReceiptView()
-                .let(this.findNavController()::navigate)
+    private fun setNavigation() =
+        DestinationsAdapter(this).attachToNav(home_nav, home_destinations_host)
+
+    private class DestinationsAdapter(parent: Fragment): FragmentStateAdapter(parent) {
+        private val destinations = listOf(
+            { DashboardFragment() },
+            { PendingFragment() },
+            { ExportsFragment() },
+            { SettingsFragment() }
+        )
+
+        override fun getItemCount() = destinations.size
+
+        override fun createFragment(position: Int) =
+            destinations[position].invoke()
+
+        fun attachToNav(navigationView: BottomNavigationView, pager: ViewPager2) {
+            pager.apply {
+                adapter = this@DestinationsAdapter
+                isUserInputEnabled = false
+            }
+            navigationView.setOnNavigationItemSelectedListener {
+                val index = when(it.itemId) {
+                    R.id.dashboard -> 1
+                    R.id.pending -> 2
+                    R.id.exports -> 3
+                    R.id.settings -> 4
+                    else -> null
+                }
+                index?.let(pager::setCurrentItem)
+                true
+            }
         }
     }
 }
