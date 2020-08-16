eda78e082a7e0a3f0d2fd86f4232090f6b8c878d
lucianbc <lucianboaca97@gmail.com>
2020-08-16 15:26:06 +0100

    Added button to navigate to scanner

--- receiptscan/src/main/java/com/lucianbc/receiptscan/home/DashboardFragment.kt
+++ receiptscan/src/main/java/com/lucianbc/receiptscan/home/DashboardFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.lucianbc.receiptscan.HomeFragmentDirections
 import com.lucianbc.receiptscan.R
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 
 class DashboardFragment : Fragment() {
 
@@ -15,5 +18,11 @@ class DashboardFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        openScannerBtn.setOnClickListener {
+            this.findNavController().navigate(HomeFragmentDirections.actionHomeToScanner())
+        }
     }
 }
