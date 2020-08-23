ae940258037413737d28d3ebb3ebe0a70b1527fe
lucianbc <lucianboaca97@gmail.com>
2020-08-23 17:04:14 +0100

    Added permission checking

--- receiptscan/src/main/java/com/lucianbc/receiptscan/ScannerFragment.kt
+++ receiptscan/src/main/java/com/lucianbc/receiptscan/ScannerFragment.kt
@@ -1,18 +1,73 @@
 package com.lucianbc.receiptscan
 
+import android.Manifest
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 
-class ScannerFragment : Fragment() {
+class ScannerFragment : Fragment(), EasyPermissions.PermissionCallbacks {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_scanner, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkCameraPermission()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    @AfterPermissionGranted(CAMERA_PERMISSION_REQUEST)
+    private fun checkCameraPermission() {
+        println("Check permissions")
+        val perm = Manifest.permission.CAMERA
+        if (EasyPermissions.hasPermissions(requireActivity(), perm))
+            println("Has permission")
+        else
+            EasyPermissions.requestPermissions(
+                this,
+                getString(R.string.scanner_camera_permissions),
+                CAMERA_PERMISSION_REQUEST,
+                perm
+            )
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {}
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms))
+            AppSettingsDialog.Builder(this)
+                .setRequestCode(requestCode)
+                .build()
+                .show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            CAMERA_PERMISSION_REQUEST -> checkCameraPermission()
+        }
+    }
+
+    companion object {
+        const val CAMERA_PERMISSION_REQUEST = 100
     }
 }
