78be19d68582a83c3ebd3cdea8dd75857248b3a2
lucianbc <lucianboaca97@gmail.com>
2020-09-17 20:21:34 +0100

    Show / hide status bag

--- receiptscan/src/main/java/com/lucianbc/receiptscan/MainActivity.kt
+++ receiptscan/src/main/java/com/lucianbc/receiptscan/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.lucianbc.receiptscan
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rectToNavChange(findNavController(R.id.root_navigator))
+    }
+
+    private fun rectToNavChange(navController: NavController) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            manageStatusPar(destination)
+        }
+    }
+
+    private fun manageStatusPar(destination: NavDestination) {
+        if (destination.id == R.id.scanner)
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        else
+            window.clearFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+
     }
 }
