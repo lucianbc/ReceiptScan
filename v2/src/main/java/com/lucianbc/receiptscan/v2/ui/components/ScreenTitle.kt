9a29c76e2f716d5bff49804d977395d11dc5e84d
lucianbc <lucianboaca97@gmail.com>
2021-04-10 15:46:05 +0100

    Implemented screen title component using constraint layout

--- v2/src/main/java/com/lucianbc/receiptscan/v2/ui/components/ScreenTitle.kt
+++ v2/src/main/java/com/lucianbc/receiptscan/v2/ui/components/ScreenTitle.kt
@@ -1,34 +1,105 @@
 package com.lucianbc.receiptscan.v2.ui.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+
+
+typealias IconAction = Pair<ImageVector, () -> Unit>
 
 @Composable
-fun ScreenTitle(title: String) {
-    Row(
-        modifier = Modifier.then(Modifier.fillMaxWidth())
-            .then(Modifier.padding(top = 64.dp, bottom = 16.dp)),
-        horizontalArrangement = Arrangement.Center,
+fun ScreenTitle(
+    title: String,
+    icons: List<IconAction> = emptyList(),
+) {
+    val constraintSet = ConstraintSet {
+        val leftEnd = createGuidelineFromStart(16.dp)
+        val rightEnd = createGuidelineFromEnd(16.dp)
+        val titleRef = createRefFor("title")
+        constrain(titleRef) {
+            linkTo(start = leftEnd, end = rightEnd, bias = 0.5f)
+            top.linkTo(parent.top, 64.dp)
+            if (icons.isEmpty())
+                bottom.linkTo(parent.bottom, 16.dp)
+        }
+        if (icons.isNotEmpty()) {
+            val iconRefs = Array(icons.size) { createRefFor("icon-$it") }
+            createHorizontalChain(*iconRefs, chainStyle = ChainStyle.Packed)
+            iconRefs.forEachIndexed { ix, it ->
+                constrain(it) {
+                    if (ix == 0) {
+                        start.linkTo(titleRef.start)
+                    } else {
+                        start.linkTo(iconRefs[ix-1].end, 16.dp)
+                    }
+                    if (ix == icons.size - 1) {
+                        end.linkTo(titleRef.end)
+                    }
+
+                    top.linkTo(titleRef.bottom, 16.dp)
+                    bottom.linkTo(parent.bottom, 16.dp)
+                }
+            }
+        }
+    }
+
+    ConstraintLayout(
+        constraintSet = constraintSet,
+        modifier = Modifier
+            .fillMaxWidth()
     ) {
+
         Text(
-            fontSize = 36.sp,
-            fontFamily = FontFamily.SansSerif,
             text = title,
+            style = MaterialTheme.typography.h4,
+            modifier = Modifier
+                .layoutId("title"),
         )
+
+        icons.forEachIndexed { ix, it ->
+            Icon(
+                it.first, contentDescription = "",
+                modifier = Modifier
+                    .clickable { it.second() }
+                    .layoutId("icon-$ix")
+                ,
+            )
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "With no icons")
 @Composable
-fun ScreenTitlePreview() {
-    ScreenTitle(title = "Title here")
-}
+fun ScreenBigTitlePreview() = ScreenTitle(title = "Exports")
+
+@Preview(showBackground = true, name = "With one icon")
+@Composable
+fun ScreenBigTitleWithIconPreview() = ScreenTitle(title = "Exports", icons = listOf(
+    Icons.Default.Add to {},
+))
+
+@Preview(showBackground = true, name = "With multiple icon")
+@Composable
+fun ScreenBigTitleWithIconsPreview() = ScreenTitle(title = "Exports", icons = listOf(
+    Icons.Default.Add to {},
+    Icons.Default.Search to {},
+    Icons.Default.Share to {},
+    Icons.Default.AccountCircle to {},
+))
+
