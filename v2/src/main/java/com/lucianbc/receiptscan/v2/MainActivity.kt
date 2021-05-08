80415a1199982ab7b748dec7e2493090ea571a60
lucianbc <lucianboaca97@gmail.com>
2021-05-08 14:27:49 +0100

    Fix weird IDE error

--- v2/src/main/java/com/lucianbc/receiptscan/v2/MainActivity.kt
+++ v2/src/main/java/com/lucianbc/receiptscan/v2/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.lucianbc.receiptscan.v2
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.lucianbc.receiptscan.v2.ui.ComposableSampleTheme
-import com.lucianbc.receiptscan.v2.ui.components.ScreenTitle
-import androidx.activity.compose.setContent
 import com.lucianbc.receiptscan.v2.ui.Navigation
-import com.lucianbc.receiptscan.v2.ui.screens.DraftsScreen
 
 class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingSuperCall")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
