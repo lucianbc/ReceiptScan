e79044bb854e7ed43d49ce368df5af01db23b3c1
lucianbc <lucianboaca97@gmail.com>
2021-06-04 15:28:24 +0100

    Expose column scope to screen children

--- v2/src/main/java/com/lucianbc/receiptscan/v2/ui/components/Screen.kt
+++ v2/src/main/java/com/lucianbc/receiptscan/v2/ui/components/Screen.kt
@@ -1,15 +1,19 @@
 package com.lucianbc.receiptscan.v2.ui.components
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.lucianbc.receiptscan.v2.ui.ComposableSampleTheme
 
 @Composable
-fun Screen(content: @Composable () -> Unit) {
+fun Screen(content: @Composable ColumnScope.() -> Unit) {
     ComposableSampleTheme {
-        Column(modifier = Modifier.fillMaxHeight().fillMaxWidth()) {
+        Column(modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()) {
             content()
         }
     }
